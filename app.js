ba4f74d3a698c5315c1e67512bb89f05dd7e5746
srinivasan0077 <srinivasandhandapani071201@gmail.com>
2021-06-30 22:33:03 +0530

    Initial commit Its my first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+
+const named=document.getElementById("name");
+const rollno=document.getElementById("rno");
+
+
+const setDetails=(m,n)=>{
+       named.innerText=m;
+       rollno.innerText=n;
+}
+
+
+setDetails("srinivasan",2345)
